225228616d7996fa3925ca415e3f9222b85a6f43
HanifahS19 <hanifahsabdani@gmail.com>
2024-12-16 20:42:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+// Sesuaikan dengan setting MySQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "penduduk_db";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query untuk mengambil data dari tabel penduduk
+$sql = "SELECT * FROM penduduk";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1px'>
+            <tr>
+                <th>Kecamatan</th>
+                <th>Longitude</th>
+                <th>Latitude</th>
+                <th>Luas</th>
+                <th>Jumlah Penduduk</th>
+            </tr>";
+
+    // Output data tiap baris
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row["kecamatan"] . "</td>
+                <td>" . $row["longitude"] . "</td>
+                <td>" . $row["latitude"] . "</td>
+                <td>" . $row["luas"] . "</td>
+                <td align='right'>" . $row["jumlah_penduduk"] . "</td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+// Menutup koneksi
+$conn->close();
